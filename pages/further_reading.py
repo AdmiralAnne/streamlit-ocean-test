91d9b44d357cf9812382e1d92751a8e9576dda74
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-17 23:55:00 +0530

    test

--- pages/further_reading.py
+++ pages/further_reading.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.success("Collection of some resources and links where you can discover more about the Big Five model.")
+
 with st.sidebar:
     st.success('Navigate to more pages using the above links ⬆️')
     
@@ -8,6 +9,7 @@ if st.button("Home"):
     st.switch_page("streamlit_app.py")
 if st.button("Read more"):
     st.switch_page("pages/further_reading.py")
+
 st.markdown("""
 
     1. [Simple Psychology - Big Five Personality](https://www.simplypsychology.org/big-five-personality.html)
