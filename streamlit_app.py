37223015b2d297cd2ec8d7462dc260b2e87de586
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-17 18:07:09 +0530

    addeed Expander

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,21 +7,11 @@ st.info('A minimal way to take the OCEAN Test and get insights into your persona
 
 # import the csv dataset and read the file
 data=pd.read_csv("OCEAN_questions.csv")
-data
-
 questions=data.loc[:, ["ID", "question","choice"]]
-questions
 
-# st.radio("**Question number 1**", ["1","2","3","4","5"], index=0, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, horizontal=False, captions=[
-#        "Disagree strongly",
-#        "Disagree a little",
-#        "Neither agree nor disagree",
-#        "Agree a little",
-#        "Agree strongly"], label_visibility="visible")
-
-
-import streamlit as st
-import pandas as pd
+with st.expander("Data"):
+    data
+    questions
 
 def display_questions(df):
   """Displays questions with radio buttons for answers and stores choices in DataFrame.
