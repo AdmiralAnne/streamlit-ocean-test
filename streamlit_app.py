fdbba432dfabb540b6e11c525a20157417a6969e
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-17 23:33:12 +0530

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -212,6 +212,7 @@ with st.sidebar:
 
     ### link to websites:
     1. [Simple Psychology - Big Five Personality](https://www.simplypsychology.org/big-five-personality.html)
+    <hr>
     2. [Very well mind - How to Use the Big 5 Personality Traits](https://www.verywellmind.com/the-big-five-personality-dimensions-2795422)
 
     """
