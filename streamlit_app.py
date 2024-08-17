e68d8094ba3ca674b0e31b887780faa7b1a6b764
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-17 17:32:31 +0530

    added variable dataframe "data"

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,7 @@ import pandas as pd
 st.title('OCEAN Test App')
 
 st.info('A minimal way to take the OCEAN Test and get insights into your personality!')
+
+# import the csv dataset and read the file
+data = pd.read_csv("OCEAN_questions.csv")
+data
