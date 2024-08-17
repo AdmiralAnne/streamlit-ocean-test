6e06b9135baaa2ee8663a31900ca56f8f8aeda75
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-17 15:03:50 +0530

    debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ st.title('OCEAN Test App')
 
 st.info('A minimal way to take the OCEAN Test and get insights into your personality!')
 df=pd.read_csv("OCEAN_questions - give me the sheet here as a table.csv")
-df.rename(columns={'score': 'choice'})
-df
+data=df.rename(columns={'score': 'choice'})
+data
