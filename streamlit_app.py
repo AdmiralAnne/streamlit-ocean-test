e408e671033cdecda8aa04463ff44e2e992d3d52
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-17 22:20:47 +0530

    testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -263,4 +263,6 @@ def get_interpretation(trait, score):
 
 # Iterate through each row in the DataFrame and display the interpretation
 for index, row in scores_df.iterrows():
+    index
+    row
     get_interpretation(row['Trait'], row['Score'])
