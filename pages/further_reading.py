2bf24d51b1936cb582423d33ec1878d7468bcafc
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-18 00:18:57 +0530

    test

--- pages/further_reading.py
+++ pages/further_reading.py
@@ -1,20 +1,15 @@
 import streamlit as st
 st.success("Collection of some resources and links where you can discover more about the Big Five model.")
 
-with st.sidebar:
-    st.success('Navigate to more pages using the above links ⬆️')
-    
-
-if st.button("Home"):
-    st.switch_page("streamlit_app.py")
-if st.button("Read more"):
-    st.switch_page("pages/further_reading.py")
-
 st.markdown("""
 
     1. [Simple Psychology - Big Five Personality](https://www.simplypsychology.org/big-five-personality.html)
     
     2. [Very well mind - How to Use the Big 5 Personality Traits](https://www.verywellmind.com/the-big-five-personality-dimensions-2795422)
+
+    3. [Scontrino-powell- Personality-and-job-performance](https://scontrino-powell.com/blog/personality-and-job-performance)
+
+    4. [Big Five Test - Test/Read more](https://bigfive-test.com/)
 
     """
     )
