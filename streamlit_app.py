114df8696797eee066b21dd248b38566b3633762
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-17 17:43:47 +0530

    added some options

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,9 @@ data
 questions=data.loc[:, ["ID", "question"]]
 questions
 
-st.radio("Question number 1", "One,Two,Three", index=0, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, horizontal=False, captions=None, label_visibility="visible")
+st.radio("Question number 1", ["1","2","3","4","5"], index=0, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, horizontal=False, captions=[
+        "Disagree strongly",
+        "Disagree a little",
+        "Neither agree nor disagree",
+        "Agree a little",
+        "Agree strongly"], label_visibility="visible")
