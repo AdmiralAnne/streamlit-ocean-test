6460ea8bac4dfdae1c922866606eea2232ab200b
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-17 17:31:27 +0530

    a hard reset

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,3 @@ import pandas as pd
 st.title('OCEAN Test App')
 
 st.info('A minimal way to take the OCEAN Test and get insights into your personality!')
-
-df=pd.read_csv("OCEAN_questions.csv")
-df
