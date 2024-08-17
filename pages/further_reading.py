7c454be828b51807ff94058e6d0946fc2fd9ddf5
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-08-17 23:52:15 +0530

    test

--- pages/further_reading.py
+++ pages/further_reading.py
@@ -1,9 +1,8 @@
 import streamlit as st
-st.title("this is a title")
+st.success("Collection of some resources and links where you can discover more about the Big Five model.")
 
 st.markdown("""
 
-    ### link to websites:
     1. [Simple Psychology - Big Five Personality](https://www.simplypsychology.org/big-five-personality.html)
     
     2. [Very well mind - How to Use the Big 5 Personality Traits](https://www.verywellmind.com/the-big-five-personality-dimensions-2795422)
